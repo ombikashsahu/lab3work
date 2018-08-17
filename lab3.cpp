87337651a147abc0506c7b0ecd65f3ed7bdc8707
ombikashsahu <42239244+ombikashsahu@users.noreply.github.com>
2018-08-17 06:58:43 +0000

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int sum = 4+6;
+cout <<sum<<endl;
+cout <<sizeof(sum)<<endl;
+char a = 23;
+cout <<sizeof(a)<<endl;
+bool b = true;
+cout <<sizeof(b)<<endl;
+float c= 7.33;
+cout <<sizeof(c)<<endl;
+double d = 33.55;
+cout <<sizeof(d)<<endl;
+}
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+int main(){
+int sum = 5+7;
+cout <<sum<<endl;
+int difference =10-8;
+cout <<difference<<endl;
+int multiply =2*3;
+cout <<multiply<<endl;
+int divide =20/5;
+cout <<divide<<endl;
+int modulus=30%7;
+cout <<modulus<<endl;
+}
